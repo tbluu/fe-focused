288e361e18f84730ecc1b35521b3e88b936f1fed
bejita <5802bejita@gmail.com>
2025-12-28 05:51:31 +0700

    path render-1

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/focused-connect",
+  baseURL: "https://be-focused.onrender.com/focused-connect",
   headers: {
     "Content-Type": "application/json",
   },
